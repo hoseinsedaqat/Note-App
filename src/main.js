2a98ba63cbd337ca56007c5d9048e0b046bbdddc
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-07-29 05:01:45 +0430

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
