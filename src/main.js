49d5891f1abfeb23c408db760740e8abf35275a3
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-07-29 05:01:28 +0430

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
-import router from './router'
+import router from './router';
 import store from './store'
 import vuetify from './plugins/vuetify';
 
